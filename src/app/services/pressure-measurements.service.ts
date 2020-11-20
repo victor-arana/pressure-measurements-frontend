d94a83f7b9ee0459eca61cd4176e8193bbec4cf9
victor-arana <victor_arana@hotmail.com>
2020-11-20 13:34:28 -0600

    :measurements-service::implementation: implement methods
    Changes committed:
    	modified:   src/app/services/pressure-measurements.service.ts

--- src/app/services/pressure-measurements.service.ts
+++ src/app/services/pressure-measurements.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PressureMeasurementsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get('pressureMeasurements');
+  }
 }
